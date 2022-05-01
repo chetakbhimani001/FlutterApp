46fe561a3f0c9978175e64b257d89c90a4019717
Amit <amit@gmail.com>
2022-05-01 19:44:21 +0530

    added api

--- lib/screens/music_dashboard/music_list.dart
+++ lib/screens/music_dashboard/music_list.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:music_player/utils/custom_color.dart';
 
 class MusicList extends StatefulWidget {
   const MusicList({Key? key}) : super(key: key);
@@ -11,25 +13,70 @@ class MusicList extends StatefulWidget {
 }
 
 class _MusicListState extends State<MusicList> {
+  late Future<List<Photos>> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("API Data"),
+              backgroundColor: CustomColor.primaryColor,
+            ),
+            body: Center(
+                child: FutureBuilder<List<Photos>>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Center(
+                    child: ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(snapshot.data![index].title.toString()),
+                        );
+                      },
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ))));
   }
 }
 
-Future<Photos> fetchAlbum() async {
+Future<List<Photos>> fetchAlbum() async {
   final response = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1/photos'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Photos.fromJson(jsonDecode(response.body));
+    final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
+
+    return parsed.map<Photos>((json) => Photos.fromJson(json)).toList();
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load album');
   }
+}
+
+// A function that converts a response body into a List<Photo>.
+List<Photos> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Photos>((json) => Photos.fromJson(json)).toList();
 }
 
 class Photos {
@@ -46,13 +93,13 @@ class Photos {
     required this.url,
     required this.thumbnailUrl,
   });
-
   factory Photos.fromJson(Map<String, dynamic> json) {
     return Photos(
-        albumId: json['albumId'],
-        id: json['id'],
-        title: json['title'],
-        url: json['url'],
-        thumbnailUrl: json['thumbnailUrl']);
+      albumId: json['albumId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      url: json['url'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
+    );
   }
 }
